7803487c3450a815624d6fe097e7b044089910bc
Ruben Verhoef <ruben.leopold@gmail.com>
2026-08-04 14:54:45 +0200

    first version hero section portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
 	import me from '$lib/assets/Me.avif';
+	import LinkedIn from 'virtual:icons/fa7-brands/linkedin';
+	import GitHub from 'virtual:icons/fa7-brands/github';
 
+	const linkedInUrl = 'https://www.linkedin.com/in/ruben-verhoef-00bb47267/';
+	const gitHubUrl = 'https://github.com/Made-By-Ruben';
 </script>
 
-<main class="h-screen w-full p-5">
-	<h1 class="text-3xl font-bold">Hi, I'm Ruben.</h1>
-	<h2>24yo software engineer from The Netherlands 🇳🇱</h2>
-	<!-- <img src={me} class="rounded-2xl h-44"/> -->
+<main class="flex h-screen w-full flex-col items-center p-5">
+	<img src={me} class="h-44 rounded-full pb-5" />
+	<div class="flex">
+		<a href={linkedInUrl} target="_blank">
+			<LinkedIn />
+		</a>
+		<a href={gitHubUrl} target="_blank">
+			<GitHub />
+		</a>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="text-3xl font-bold">Hi, I'm Ruben.</h1>
+		<h2 class="pb-1">A 24yo software engineer from The Netherlands 🇳🇱</h2>
+		<div class="flex gap-1 h-5">
+			<img class="rounded" src="https://shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=FFF&style=flat-square" alt="">
+			<img class="rounded" src="https://img.shields.io/badge/-SvelteKit-black?style=for-the-badge&logo=svelte&color=orangered&logoColor=white" alt="">
+			<img class="rounded" src="https://img.shields.io/badge/node.js-339933?style=for-the-badge&logo=Node.js&logoColor=white" alt="">
+			<img class="rounded" src="https://img.shields.io/badge/express.js-000000?style=for-the-badge&logo=express&logoColor=white" alt="">
+		</div>
+	</div>
 </main>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite'
+
 
 export default defineConfig({
 	plugins: [
+		Icons({ compiler: 'svelte' }),
 		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
