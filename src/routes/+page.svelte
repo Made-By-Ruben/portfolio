a8649845d447963e7b3eee8191d5f605a90b93f5
FalloutCS <ruben.leopold@gmail.com>
2026-08-04 13:17:52 +0200

    researching icon packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<main class="w-full h-screen p-5">
+<script lang="ts">
+	import me from '$lib/assets/Me.avif';
+
+</script>
+
+<main class="h-screen w-full p-5">
 	<h1 class="text-3xl font-bold">Hi, I'm Ruben.</h1>
 	<h2>24yo software engineer from The Netherlands 🇳🇱</h2>
-    
+	<!-- <img src={me} class="rounded-2xl h-44"/> -->
 </main>
