9ccb631fd4ba4aae5051330109e8dbe192eda6c0
FalloutCS <ruben.leopold@gmail.com>
2026-08-11 10:57:10 -0700

    worked on globalising styling

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,8 +1,17 @@
+import rolHero from "$lib/assets/projectRolHero.avif"
+
 type project = {
     title: string;
     heroImage: string;
     description: string;
     techStack: string[];
-    links: string[];
+    links: Record<string, string>
 }
 
+export const projects: project[] = [{
+    title: "RoL liveStats",
+    description: "Full stack applicatie wat GameWaves in staat stelt data uit hun League of Legends toernooien te verwerken tot visuele elementen in de live-uitzending. De applicatie wordt momenteel gebruikt in twee officiële regionale toernooien, de Road of Legends en de NLC.",
+    heroImage: rolHero,
+    links: {repo: "https://github.com/Made-By-Ruben/liveStatsDashboard", architecture: ""},
+    techStack: ["TypeScript", "Node.js", "Express.js", "MySQL", "Redis", "Zod", "SvelteKit", "Vite", "TailwindCSS"]
+}]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,14 @@
 	import GitHub from 'virtual:icons/fa7-brands/github';
 	import TechBadges from '$lib/components/TechBadges.svelte';
 	import Experience from '$lib/components/Experience.svelte';
+	import { projects } from '$lib/data/projects';
 
 	const linkedInUrl = 'https://www.linkedin.com/in/ruben-verhoef-00bb47267/';
 	const gitHubUrl = 'https://github.com/Made-By-Ruben';
 </script>
 
 <main class="flex h-screen w-full flex-col items-center gap-5 p-10">
-	<header class="flex flex-col items-center">
+	<section class="section items-center gap-0">
 		<img src={me} class="h-44 rounded-full pb-5" />
 		<div class="flex">
 			<a href={linkedInUrl} target="_blank">
@@ -23,10 +24,27 @@
 
 		<div class="flex flex-col items-center">
 			<h1 class="text-3xl font-bold">Hi, I'm Ruben.</h1>
-			<h2 class="pb-1">A 24yo software engineer from The Netherlands 🇳🇱</h2>
+			<p class="pb-1">A 24yo software engineer from The Netherlands 🇳🇱</p>
 			<TechBadges />
 		</div>
-	</header>
+	</section>
 
-	<Experience />
+	<section class="section">
+		<h1>My Experience</h1>
+		<Experience />
+	</section>
+
+	<section class="section">
+		<h1>My Projects</h1>
+
+		<div class="grid rounded-2xl shadow shadow-gray-400">
+			{#each projects as project}
+				<div class="p-5">
+					<img class="" src={project.heroImage} />
+					<h2>{project.title}</h2>
+					<p>{project.description}</p>
+				</div>
+			{/each}
+		</div>
+	</section>
 </main>
