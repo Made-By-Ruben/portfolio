755171f240bfdd215688a08d7efa92a67e712b66
FalloutCS <ruben.leopold@gmail.com>
2026-08-17 15:32:26 -0700

    Wrote about the road of legend liveStats indepth

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
-    import type { PageProps } from './$types';
+	import type { PageProps } from './$types';
 
-    let { data }: PageProps = $props();
+	let { data }: PageProps = $props();
+	let projectSummary = $derived(data.project.summary);
+	let projectDetails = $derived(data.project.details);
 </script>
+
+<section class="section">
+	<h1 class="text-3xl">{projectSummary.title}</h1>
+	<p>{projectSummary.description}</p>
+	<div class="flex flex-wrap gap-2 pb-1">
+		{#each projectSummary.techStack as tech}
+			<span class="rounded bg-gray-200 px-2 py-0.5 text-xs font-bold">{tech}</span>
+		{/each}
+	</div>
+	<div class="grid gap-1 lg:grid-cols-2">
+		<img  class="rounded-2xl" src={projectSummary.heroImage} />
+		<img class="rounded-2xl" src={projectSummary.secondImage}>
+	</div>
+</section>
+
+{#each projectDetails as detail}
+	<section class="section">
+		<h2>{detail.sectionTitle}</h2>
+		{#each detail.sectionBody as paragraph}
+			<p>{paragraph}</p>
+		{/each}
+		<img class="rounded-2xl border border-gray-400" src={detail.image} />
+	</section>
+{/each}
